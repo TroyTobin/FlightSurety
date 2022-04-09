3ff55dcfdb8e6474f51dd13e0bea508edf47712e
Troy Tobin <troy.d.tobin@gmail.com>
2022-04-09 23:50:35 +1000

    Adjustments to make webpack config work correctly

--- webpack.config.dapp.js
+++ webpack.config.dapp.js
@@ -1,58 +1,47 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack')
 
 module.exports = {
-  entry: ['babel-polyfill', path.join(__dirname, "src/dapp")],
+  entry:  "./src/dapp/index.js",
   output: {
-    path: path.join(__dirname, "prod/dapp"),
-    filename: "bundle.js"
+    path: path.resolve(__dirname, "src/dapp"),
+    filename: "main.js"
   },
   module: {
     rules: [
-    {
-        test: /\.(js|jsx)$/,
-        use: "babel-loader",
-        exclude: /node_modules/
-      },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.html$/,
-        use: "html-loader",
-        exclude: /node_modules/
-      }
-    ]
-  },
+    ],
+  },  
   plugins: [
-    new HtmlWebpackPlugin({ 
-      template: path.join(__dirname, "src/dapp/index.html")
-    })
+      // https://stackoverflow.com/questions/68707553/uncaught-referenceerror-buffer-is-not-defined
+      // Needed otherwise get a "Brocess is not defined" error:
+      new webpack.ProvidePlugin({
+          Buffer: ['buffer', 'Buffer'],
+      }),
+      // https://stackoverflow.com/questions/41359504/webpack-bundle-js-uncaught-referenceerror-process-is-not-defined
+      // Needed otherwise get a "process is not defined" error:
+      new webpack.ProvidePlugin({
+          process: 'process/browser',
+    }),
   ],
-  resolve: {
-    extensions: [".js"],
+  resolve:
+  {
     fallback: {
-      "assert": false,
-      "crypto": false,
-      "http": false,
-      "https": false,
-      "os": false,
-      "stream": false,
-      "url": false
-    } 
+      "assert": require.resolve("assert/"),
+      "crypto": require.resolve("crypto-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "stream": require.resolve("stream-browserify")
+    }
   },
   devServer: {
-    static: path.join(__dirname, "dapp"),
-    port: 8000,
-    devMiddleware: {
-      stats: "minimal"
-    }
+    static: { 
+      directory: path.join(__dirname, "src/dapp")
+    },
+    port: 8000
   },
 };
